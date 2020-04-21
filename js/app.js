18392dcc4560b01a160bca526813b2f8488ebdad
Melissa Galush <galushmn@gmail.com>
2020-04-20 17:16:43 -0700

    created template for app.js and results.js

---
+++ js/app.js
@@ -0,0 +1,95 @@
+// TODO: create userArray
+
+
+
+// TODO: create colorArray for balloons and favorite color (two colors for now)
+
+
+
+// TODO: create random color generator
+    // TODO: use random number generator to pick random color from array
+
+
+
+// TODO: create constructor for Balloons
+    // color will be rendered from color array; will not be "this.color"
+    // for example: this.color = colorArray;
+
+
+
+// TODO: create new Balloon objects - one for each color in the color array
+
+
+
+// TODO: create constructor for user
+    // TODO: within constructor, push user into userArray using 'includes' method (to avoid duplicates)
+
+
+
+// TODO: create renderButton function
+
+
+
+// TODO: create renderCSSOnElement function
+
+
+
+// TODO: create renderCountdown function
+
+
+
+// TODO: create renderTotalScore function
+
+
+
+// TODO: create renderAll function
+    // TODO: invoke all render functions
+
+
+
+// TODO: create event listener for form
+
+
+
+// TODO: create an event handler for form
+      // TODO: once submit is clicked, clear page
+      // TODO: call renderAll function
+
+
+
+// TODO: create renderBalloons function
+
+
+
+// TODO: create startTimer function
+
+
+
+// TODO: add event listener for start button
+    // TODO: invoke renderBalloons function
+    // TODO: invoke startTimer functions
+    // TODO: invoke startGame function
+
+
+
+// TODO: create addEventListener function for clicking of balloons
+
+
+
+// TODO: create startGame function
+    // TODO: when balloon is clicked, remove from array
+    // TODO: if color === 'selected-color', add points to score
+    // TODO: else color !== 'selected-color', remove points from score
+
+
+
+// TODO: create endGame function
+    // TODO: push totalScore into array
+    // TODO: store totalScore array in local storage
+    // TODO: remove all event listeners
+    // TODO: send user to results page
+
+
+    
+// TODO: invoke endGame function
+    // TODO: delay start of invocation of function
---
+++ js/results.js
@@ -0,0 +1,18 @@
+// TODO: create function renderResults for all of the following:
+
+
+
+  // TODO: get results from local storage
+     // TODO: get & parse
+
+
+
+  // TODO: create table for results
+
+
+
+  // TODO: put results inside table
+
+
+
+  // TODO: render table
