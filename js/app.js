2a74a32263018e1df1f9cf7ac374de574c903cc7
BryantDavis1986 <bryant.davis@hotmail.com>
2020-04-21 11:34:56 -0700

    created the function for creating balloon, created imgSrc array and balloonObject array

--- js/app.js
+++ js/app.js
@@ -6,8 +6,20 @@ var userArray = [];
 
 // DONE: create colorArray for balloons (two colors for now)
 // will add more once some testing is completed
-var colorArray = ['red', 'blue'];
+// changed to assign to constructor to make it clear what the array belongs to
+Balloon.colorArray = ['red', 'blue'];
 
+// DONE: create imageSrcArray for balloons (two colors for now)
+// will add more once some testing is completed
+// changed to assign to constructor to make it clear what the array belongs to
+Balloon.imageSrcArray = ['assets/red-balloon.png', 'assets/blue-balloon.png'];
+
+// DONE: create balloonArray to store all balloon objects
+// will add more balloon objects are more colors and image src are added into the other arrays. 
+// refer to createBalloon function
+// changed to assign to constructor to make it clear what the array belongs to
+
+Balloon.balloonArray = [];
 
 
 // TODO: create random color generator
@@ -18,17 +30,26 @@ var colorArray = ['red', 'blue'];
 // DONE: create constructor for Balloons
     // color will be rendered from color array; will not be "this.color = color;"
     // for example: this.color = colorArray;
-function Balloon (iColor, imageSrc) {
+function Balloon (color, imageSrc) {
     // this.color is the index of the colorArray to make it dynamic
-    this.color = colorArray[iColor];
-    //this.image is the source of the image
-    this.image = imageSrc;
+    this.color = Balloon.colorArray[color];
+    //this.image is the source of the image in imageSrcArray(same index as the color it is assigned to)
+    this.imageSrc = Balloon.imageSrcArray[imageSrc];
 }
 
 
 
-// TODO: create new Balloon objects - one for each color in the color array
-
+// DONE: create new Balloon objects - one for each color in the color array
+// this function will create all balloon objects for as long as there is information in the 2 referenced arrays
+// then it will push the objects into the balloon array
+// no return is needed for this function, if you need access to the objects reference the balloonArray
+function createBalloon() {
+    for(i in Balloon.colorArray) {
+        var myBalloon = new Balloon(i,i);
+        Balloon.balloonArray.push(myBalloon);
+    }
+}
+createBalloon();
 
 
 // TODO: create constructor for user
